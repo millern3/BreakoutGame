2618dcfe13da20fe1f3191d5d6f00c0c0f87f1ed
Unknown <millern2@hawkmail.newpaltz.edu>
2017-03-06 10:49:48 -0500

    Added Game Over and 3 Lives

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
     public Vector2 startingVelocity = new Vector2(15, -20);
+    private Vector3 startingPosition;
+    public GameObject gameOver;
+
+    int lives = 3;
 	// Use this for initialization
 	void Start () {
+        startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;  
 	}
 	
@@ -15,10 +20,26 @@ public class Ball : MonoBehaviour {
 		if(transform.position.y < -5.8f) {
             GetOut();
         }
-	}
+        if (Input.GetButtonDown("Jump")) {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }
+    }
 
     void GetOut()
     {
         Debug.Log("You are out");
+        lives = lives - 1;
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+
+        if (lives == 0)  {
+            DoGameOver();
+        }
+    }
+
+    void DoGameOver()
+    {
+              gameOver.SetActive(true);
     }
 }
