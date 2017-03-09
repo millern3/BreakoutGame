48b8206c1930dc503e19e3bf1671cb0c3a8884ad
Unknown <millern2@hawkmail.newpaltz.edu>
2017-03-09 07:35:11 -0500

    New Scripts and scenes

---
+++ Assets/Scripts/Brick L2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickL2 : MonoBehaviour {
+
+    public GameObject Brickbreaker;
+    int Brick = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    
+    Debug.Log("Strong Brick");
+        Brick = Brick - 1;
+    
+        if (Brick == 0)
+    {
+        DoBrickBreak();
+    }
+
+    void DoBrickBreak()
+        {
+        Brickbreaker.SetActive(true);
+    }
+  
