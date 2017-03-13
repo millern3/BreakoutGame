5678327f6909d48e62ac5c1f1e95f506144e4d26
Unknown <millern2@hawkmail.newpaltz.edu>
2017-03-13 18:22:48 -0400

    Added new scripts and 2Levels

--- Assets/Scripts/Ball2.cs
+++ Assets/Scripts/Ball2.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Ball : MonoBehaviour {
+public class Ball2 : MonoBehaviour {
 
-    public Vector2 startingVelocity = new Vector2(15, -20);
+    public Vector2 startingVelocity = new Vector2(8, -15);
     private Vector3 startingPosition;
     public GameObject gameOverSign;
-    public GameObject youWinSign;
+    public GameObject YouWin;
     public Text livesValue;
     public Text pointsValue;
 
     int lives = 3;
     int points = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
         livesValue.text = lives.ToString();
@@ -24,14 +24,15 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position.y < -5.7f) {
+        if (transform.position.y < -3.2f)
+        {
             GetOut();
         }
-        if (Input.GetButtonDown("Jump")) {
+		if (Input.GetButtonDown("Jump"))
+        {
             GetComponent<Rigidbody2D>().velocity = startingVelocity;
         }
-    }
-
+	}
     void GetOut()
     {
         Debug.Log("You are out");
@@ -41,24 +42,24 @@ public class Ball : MonoBehaviour {
         transform.position = startingPosition;
         GetComponent<Rigidbody2D>().velocity = new Vector2();
 
-        if (lives == 0) {
+        if (lives == 0)
+        {
             DoGameOver();
         }
     }
-
     void DoGameOver()
     {
         gameOverSign.SetActive(true);
     }
-    public void BrickBroken()
+    public void BrickSmash()
     {
         points += 1;
         pointsValue.text = points.ToString();
 
-        var bricksLeft = FindObjectsOfType<Brick>().Length;
-        if(bricksLeft == 0)
+        var bricksleft = FindObjectsOfType<Brick>().Length;
+        if(bricksleft == 0)
         {
-            youWinSign.SetActive(true);
+            YouWin.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/Ball3.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball3 : MonoBehaviour {
+    public Vector2 startingVelocity = new Vector2(5, -15);
+    private Vector3 startingPosition;
+    public GameObject gameOverSign;
+    public GameObject youWinSign;
+    public Text livesValue;
+    public Text pointsValue;
+
+    int lives = 4;
+    int points = 0;
+
+	// Use this for initialization
+	void Start () {
+        startingPosition = transform.position;
+        GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        livesValue.text = lives.ToString();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.y < -4.4f)
+        {
+            GetOut();
+        }
+        if(Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }
+	}
+    void GetOut()
+    {
+        Debug.Log("You are out");
+        lives = lives - 1;
+        livesValue.text = lives.ToString();
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+  
+        if(lives == 0)
+        {
+            DoGameOver();
+        }
+    }
+    void DoGameOver()
+    {
+        gameOverSign.SetActive(true);
+    }
+    public void BrickBroken()
+    {
+        points += 1;
+        pointsValue.text = points.ToString();
+
+        var bricksLeft = FindObjectsOfType<Brick>().Length;
+        if(bricksLeft == 0)
+        {
+            youWinSign.SetActive(true);
+        }
+    }
+}
